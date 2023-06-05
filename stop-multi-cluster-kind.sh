7aae4a56b8d4c818edb776946a9d4b99441d8591
Liz Rice <liz@lizrice.com>
2023-06-05 18:22:35 +0100

    Script robustness
    Signed-off-by: Liz Rice <liz@lizrice.com>

--- stop-multi-cluster-kind.sh
+++ stop-multi-cluster-kind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker network disconnect kind kind-registry
-kind delete cluster -n jakku
-kind delete cluster -n d-qar
+kind delete cluster --name jakku
+kind delete cluster --name d-qar
